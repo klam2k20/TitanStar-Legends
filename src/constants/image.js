69ebb11bb36cd04b761a9b5e9313769af0ed0195
klam2k20 <klam2k17@gmail.com>
2024-02-29 10:54:04 -0500

    Update constant names to use best practice naming conventions

---
+++ src/constants/image.js
@@ -0,0 +1,37 @@
+import chevronsActive from '../assets/chevrons-active.png';
+import chevronsInactive from '../assets/chevrons-inactive.png';
+import silverwareActive from '../assets/silverware-active.png';
+import silverwareInactive from '../assets/silverware-inactive.png';
+import cakeActive from '../assets/cake-active.png';
+import cakeInactive from '../assets/cake-inactive.png';
+import crownActive from '../assets/crown-active.png';
+import crownInactive from '../assets/crown-inactive.png';
+import boatActive from '../assets/boat-active.png';
+import boatInactive from '../assets/boat-inactive.png';
+import scubaActive from '../assets/scuba-active.png';
+import scubaInactive from '../assets/scuba-inactive.png';
+import lightningActive from '../assets/lightning-active.png';
+import lightningInactive from '../assets/lightning-inactive.png';
+import skullActive from '../assets/skull-active.png';
+import skullInactive from '../assets/skull-inactive.png';
+
+const IMAGES = {
+  chevronsActive,
+  chevronsInactive,
+  silverwareActive,
+  silverwareInactive,
+  cakeActive,
+  cakeInactive,
+  crownActive,
+  crownInactive,
+  boatActive,
+  boatInactive,
+  scubaActive,
+  scubaInactive,
+  lightningActive,
+  lightningInactive,
+  skullActive,
+  skullInactive,
+};
+
+export default IMAGES;
---
+++ src/constants/talentPaths.js
@@ -0,0 +1,17 @@
+import IMAGES from './image';
+
+const TALENT_PATH_1 = [
+  [IMAGES.chevronsActive, IMAGES.chevronsInactive],
+  [IMAGES.silverwareActive, IMAGES.silverwareInactive],
+  [IMAGES.cakeActive, IMAGES.cakeInactive],
+  [IMAGES.crownActive, IMAGES.crownInactive],
+];
+
+const TALENT_PATH_2 = [
+  [IMAGES.boatActive, IMAGES.boatInactive],
+  [IMAGES.scubaActive, IMAGES.scubaInactive],
+  [IMAGES.lightningActive, IMAGES.lightningInactive],
+  [IMAGES.skullActive, IMAGES.skullInactive],
+];
+
+export { TALENT_PATH_1, TALENT_PATH_2 };
