26982ca22659278dd2919763e3af3b1e63fce1aa
klam2k20 <klam2k17@gmail.com>
2024-03-03 17:01:32 -0500

    Organize imports

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { AvailablePointsProvider } from './hooks/useAvailablePoints';
-import './styles/index.css';
 import { ToastProvider } from './hooks/useToast';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
