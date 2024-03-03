cc4ec1685b547e2aab571a8e36eeb29ed6eba2e8
klam2k20 <klam2k17@gmail.com>
2024-03-02 20:18:38 -0500

    Style PointDisplay and add label

--- src/components/PointDisplay.jsx
+++ src/components/PointDisplay.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { MAX_AVAILABLE_POINTS, useAvailablePoints } from '../hooks/useAvailablePoints';
+import '../styles/point-display.css';
 
 function PointDisplay() {
   const { availablePoints } = useAvailablePoints();
 
   return (
-    <div>
-      {availablePoints} / {MAX_AVAILABLE_POINTS}
+    <div className='point-display'>
+      <span>
+        {MAX_AVAILABLE_POINTS - availablePoints} / {MAX_AVAILABLE_POINTS}
+      </span>
+      <h2>Points Spent</h2>
     </div>
   );
 }
